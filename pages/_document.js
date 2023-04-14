b5f28116dc9e5b4ca086e1ecf7d150d1953a1c51
Cicero, Feijo <cicero.feijo@gmail.com>
2023-04-14 16:31:57 -0300

    Update UI

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, Title } from 'next/document'
+
 
 export default function Document() {
+  const lll = 'cicero'
   return (
     <Html lang="en">
-      <Head />
+      <Head/>
       <body>
         <Main />
         <NextScript />
